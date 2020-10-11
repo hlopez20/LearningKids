28d0945e24436209f08666a41e62689dfc14deff
Hector <lopezhector967@gmail.com>
2020-10-11 11:18:54 -0600

    Learning Kids

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+/*  rutas de la clase de español*/
 Route::get('pantallaPrincipal', function () {
     return view('pantallaPrincipal');
 });
+
+
 Route::get('pantallaEspañol', function () {
     return view('Español/pantallaPrincipalDeLaClaseDeEspañol');
 });
@@ -23,7 +28,6 @@ Route::get('pantallaEspañol', function () {
 Route::get('pantallaCuentos', function () {
     return view('Español/Cuentos');
 });
-
 
 Route::get('pantallaTipos', function () {
     return view('Español/pantallaCuentos');
@@ -33,8 +37,50 @@ Route::get('trabalenguas', function () {
     return view('Español/pantallaTrabalenguas');
 });
 
-
 Route::get('refranes', function () {
     return view('Español/pantallaRefranes');
 });
+
+Route::get('adivinanzas', function () {
+    return view('Español/adivinanzas');
+});
+
+
+Route::get('tiposAdivinanzas', function () {
+    return view('Español/adivinanzasDeAnimales');
+});
+
+
+Route::get('adivinanzaCorta', function () {
+    return view('Español/adivinanzaCorta');
+});
+
+
+Route::get('adivinanzaFruta', function () {
+    return view('Español/adivinanzasDeFrutas');
+});
+
+Route::get('adivinanzaNumeros', function () {
+    return view('Español/adivinanzasNumeros');
+});
+
+
+Route::get('adivinanzaTransporte', function () {
+    return view('Español/adivinanzaTransporte');
+});
+
+
+Route::get('chistes', function () {
+    return view('Español/pantallaChistes');
+});
+
+/*pantalla de la clase de ciencias sociales*/
+
+Route::get('cienciasSociales', function () {
+    return view('Español/pantallaPrincipalDeCienciasSociales');
+});
+
+
+
+
 
